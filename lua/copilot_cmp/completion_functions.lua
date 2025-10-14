9c6ef21ec3c517275065c8a1e0841ca372398737
Peter van der Meulen <peter@hendrikpeter.net>
2025-10-14 09:35:35 +0200

    get multiple suggestions

--- lua/copilot_cmp/completion_functions.lua
+++ lua/copilot_cmp/completion_functions.lua
@@ -8,11 +8,10 @@ local methods = {
   }
 }
 
-methods.getCompletions = function (self, params, callback)
+methods.getCompletions = function(self, params, callback)
   local respond_callback = function(err, response)
-
     if err or not response or not response.completions then
-      return callback({isIncomplete = false, items = {}})
+      return callback({ isIncomplete = false, items = {} })
     end
 
     local items = vim.tbl_map(function(item)
@@ -25,11 +24,11 @@ methods.getCompletions = function (self, params, callback)
     })
   end
 
-  api.get_completions(self.client, util.get_doc_params(), respond_callback)
-  return callback({isIncomplete = true, items = {}})
+  api.get_completions_cycling(self.client, util.get_doc_params(), respond_callback)
+  return callback({ isIncomplete = true, items = {} })
 end
 
-methods.init = function (completion_method, opts)
+methods.init = function(completion_method, opts)
   methods.opts.fix_pairs = opts.fix_pairs
   return methods[completion_method]
 end
